30b47b48618864a7c2ac52fa7637b7286daa07c0
Ben Daley <benjamindaley@gmail.com>
2015-03-07 01:51:20 +1100

    Initial commit. Working plugin, but could do with a readme, tests and examples

---
+++ index.js
@@ -0,0 +1,78 @@
+/*******************************************************************
+ *
+ *  The majority of this module is based on nunjucksify.
+ *  (https://www.npmjs.com/package/nunjucksify)
+ *
+ *  Full credit to the original authors.
+ *
+ ******************************************************************/
+
+var compiler = require('nunjucks/src/compiler');
+var Environment = require('nunjucks/src/environment').Environment;
+var env = new Environment([]);
+
+
+module.exports = function(source) {
+    this.cacheable();
+
+    var nunjucksCompiledStr = compiler.compile(source, env.asyncFilters, env.extensionsList);
+    var reg = /env\.getTemplate\(\"(.*?)\"/g;
+    var match;
+    var required = {};
+    var compiledTemplate = '';
+
+    compiledTemplate += 'var nunjucks = require( "nunjucks/browser/nunjucks-slim" );\n';
+    compiledTemplate += 'var env = nunjucks.env || new nunjucks.Environment();\n';
+
+    // Add a dependencies object to hold resolved dependencies
+    compiledTemplate += 'var dependencies = {};\n';
+
+    while( match = reg.exec( nunjucksCompiledStr ) ) {
+        var templateRef = match[1];
+        if (!required[templateRef]) {
+            // Require the dependency by name, so it get bundled in by webpack
+            compiledTemplate += 'dependencies["' + templateRef + '"] = require( "' + templateRef + '" );\n';
+            required[templateRef] = true;
+        }
+    }
+    compiledTemplate += '\n\n\n\n';
+    compiledTemplate += 'var obj = (function () {' + nunjucksCompiledStr + '})();\n';
+    compiledTemplate += 'var oldRoot = obj.root;\n';
+    compiledTemplate += 'obj.root = function( env, context, frame, runtime, cb ) {\n';
+    compiledTemplate += '	var oldGetTemplate = env.getTemplate;\n';
+    compiledTemplate += '	env.getTemplate = function( name, ec, cb ) {\n';
+    compiledTemplate += '		if( typeof ec === "function" ) {\n';
+    compiledTemplate += '			cb = ec;\n';
+    compiledTemplate += '			ec = false;\n';
+    compiledTemplate += '		}\n';
+
+    compiledTemplate += '		var _require = function(name) {\n';
+    compiledTemplate += '			try {\n';
+
+                                        // add a reference to the already resolved dependency here...
+    compiledTemplate += '				return dependencies[name];\n';
+    compiledTemplate += '			} catch (e) {\n';
+    compiledTemplate += '				if ( frame.get( "_require" ) ) return frame.get( "_require" )( name )\n';
+    compiledTemplate += '			}\n';
+    compiledTemplate += '		};\n';
+    compiledTemplate += '		var tmpl = _require( name );\n';
+    compiledTemplate += '		frame.set( "_require", _require );\n';
+    compiledTemplate += '		if( ec ) tmpl.compile();\n';
+    compiledTemplate += '		cb( null, tmpl );\n';
+    compiledTemplate += '	};';
+
+    compiledTemplate += '	oldRoot( env, context, frame, runtime, function( err, res ) {\n';
+    compiledTemplate += '		env.getTemplate = oldGetTemplate;\n';
+    compiledTemplate += '		cb( err, res );\n';
+    compiledTemplate += '	} );\n';
+    compiledTemplate += '};\n';
+
+    compiledTemplate += 'var src = {\n';
+    compiledTemplate += '	obj: obj,\n';
+    compiledTemplate += '	type: "code"\n';
+    compiledTemplate += '};\n';
+
+    compiledTemplate += 'module.exports = new nunjucks.Template( src, env );\n';
+
+    return compiledTemplate;
+}
