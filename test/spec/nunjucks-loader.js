73ea063b8deca32dfbf5fdcb87978d7abbc9c072
Olga Kozlova <for.confirmations@gmail.com>
2019-07-30 19:59:09 +0300

    Tests for relative paths

--- test/spec/nunjucks-loader.js
+++ test/spec/nunjucks-loader.js
@@ -1,12 +1,19 @@
 var nunjucks = require("nunjucks/browser/nunjucks-slim");
 
-var templateOne = require('hero/default.njk');
-var templateTwo = require('villain/default.njk');
+var templateOne = require('hero/include.njk');
+var templateTwo = require('villain/include.njk');
 
 describe('nunjucks-loader', function () {
     describe('when requiring templates with the same base name', function() {
         it('should have unique entries in nunjucksPrecompiled', function() {
             nunjucks.nunjucksPrecompiled.should.include.keys([
+                'fixtures/templates/hero/include.njk',
+                'fixtures/templates/villain/include.njk'
+            ]);
+        });
+
+        it('should have unique entries in `webpackDependencies`', function() {
+            nunjucks.webpackDependencies.should.include.keys([
                 'fixtures/templates/hero/default.njk',
                 'fixtures/templates/villain/default.njk'
             ]);
